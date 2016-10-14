5ddf8a9645cb78a61129e12600477a55fc1b85ce
Haulyn Jason <iam@haulyn.com>
2016-10-14 15:02:02 +0800

    htdp 2.5

--- main.rkt
+++ main.rkt
@@ -1,2 +1,19 @@
+#lang racket
 (+ 3 4)
 (- 1 5)
+(sqr 3)
+"hello word"
+(or #true #true)
+(and #true #false)
+(= 39 40)
+(define (sign x)
+  (cond
+    [(> x 0) 1]
+    [(= x 0) 0]
+    [(< x 0) -1]))
+(sign 3)
+(sign 0)
+(define height 60)
+height
+(string-append "what" "a" "fuck" "day")
+;;http://www.ccs.neu.edu/home/matthias/HtDP2e/part_one.html
