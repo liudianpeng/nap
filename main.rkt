55c3a5726c67c73bf30db018d653fa86d3318c8a
Haulyn Jason <saharabear@gmail.com>
2016-10-15 12:27:21 +0800

    start realm of racket

--- main.rkt
+++ main.rkt
@@ -1,19 +1,18 @@
 #lang racket
-(+ 3 4)
-(- 1 5)
-(sqr 3)
-"hello word"
 (or #true #true)
 (and #true #false)
-(= 39 40)
 (define (sign x)
   (cond
     [(> x 0) 1]
     [(= x 0) 0]
     [(< x 0) -1]))
-(sign 3)
-(sign 0)
-(define height 60)
-height
 (string-append "what" "a" "fuck" "day")
-;;http://www.ccs.neu.edu/home/matthias/HtDP2e/part_one.html
+ (require racket/gui/base)
+(define frame (new frame% [label "nap.im"]))
+(define msg (new message% [parent frame]
+                          [label "正在开发测试中"]))
+(new button% [parent frame]
+             [label "确认"]
+             [callback (lambda (button event)
+                         (send msg set-label "确认"))])
+(send frame show #t)
