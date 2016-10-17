0185bf9fbe63f4099eb87a9c50d89421d84b73ea
Haulyn Jason <iam@haulyn.com>
2016-10-17 15:22:46 +0800

    remove gui

--- main.rkt
+++ main.rkt
@@ -7,15 +7,6 @@
     [(= x 0) 0]
     [(< x 0) -1]))
 (string-append "what" "a" "fuck" "day")
- (require racket/gui/base)
-(define frame (new frame% [label "nap.im"]))
-(define msg (new message% [parent frame]
-                          [label "正在开发测试中"]))
-(new button% [parent frame]
-             [label "确认"]
-             [callback (lambda (button event)
-                         (send msg set-label "确认"))])
-(send frame show #t)
 #|
 长注释
 |#
